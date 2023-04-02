c163f5ec9ccc7c091c60c0af2bfbf638ef07c614
Liang Zhang <titanzhang@gmail.com>
2023-04-01 22:30:59 -0700

    Add LSP

--- lua/liang/packer.lua
+++ lua/liang/packer.lua
@@ -8,7 +8,7 @@ return require('packer').startup(function(use)
 		'nvim-telescope/telescope.nvim', tag = '0.1.1',
 		-- or                            , branch = '0.1.x',
 		requires = { {'nvim-lua/plenary.nvim'} }
-	}  
+	}
 
 	use({
 		'rose-pine/neovim',
@@ -29,5 +29,26 @@ return require('packer').startup(function(use)
 	use('mbbill/undotree')
 	use('tpope/vim-fugitive')
 	use('ThePrimeagen/harpoon')
-end)
 
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		requires = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},             -- Required
+			{                                      -- Optional
+			'williamboman/mason.nvim',
+			run = function()
+				pcall(vim.cmd, 'MasonUpdate')
+			end,
+		},
+		{'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+		-- Autocompletion
+		{'hrsh7th/nvim-cmp'},     -- Required
+		{'hrsh7th/cmp-nvim-lsp'}, -- Required
+		{'L3MON4D3/LuaSnip'},     -- Required
+	}
+}
+
+end)
