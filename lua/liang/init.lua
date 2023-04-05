48a879ad4c5f1059579528a5f0a9e7af436cad15
Liang Zhang <titanzhang@gmail.com>
2023-04-04 17:47:04 -0700

    Auto remove tailing whitespaces on saving

--- lua/liang/init.lua
+++ lua/liang/init.lua
@@ -1,3 +1,14 @@
 require("liang.remap")
 require("liang.config")
 require("liang.packer")
+
+local augroup = vim.api.nvim_create_augroup
+local TheLiangGroup = augroup('TheLiang', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd({ 'BufWritePre' }, {
+  group = TheLiangGroup,
+  pattern = "*",
+  command = "%s/\\s\\+$//e"
+})
