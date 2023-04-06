0b54e9db6e258ed4f0c5ab857e7b570125bbb8fd
Zhang, Liang <Liang.Zhang@elekta.com>
2023-04-06 12:53:11 -0700

    Add LSP keymap

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,28 +1,48 @@
 local loaded_lsp, _ = pcall(require, 'lsp-zero')
 
-if loaded_lsp then
-  local lsp = require('lsp-zero').preset({})
+if not loaded_lsp then
+  print('lsp-zero not installed')
+  return
+end
 
-  lsp.ensure_installed({
-    'csharp_ls',
-    'eslint',
-    'tsserver',
-    'lua_ls',
-    'html',
-    'marksman',
-  })
+local lsp = require('lsp-zero').preset({})
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({ buffer = bufnr })
 
-  require('lspconfig').lua_ls.setup({
-    settings = {
-      Lua = {
-        diagnostics = {
-          globals = { 'vim' }
-        }
+  -- List all references in telescope
+  vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', {buffer = true})
+
+  -- Go to implemention, or list them in telescope
+  vim.keymap.set('n', 'gi', function()
+    require('harpoon.mark').add_file()
+    require('telescope.builtin').lsp_implementations()
+  end, {buffer = true})
+
+  -- Go to definition, or list them in telescope
+  vim.keymap.set('n', 'gd', function()
+    require('harpoon.mark').add_file()
+    require('telescope.builtin').lsp_definitions()
+  end, {buffer = true})
+
+end)
+
+lsp.ensure_installed({
+  'csharp_ls',
+  'eslint',
+  'tsserver',
+  'lua_ls',
+  'html',
+  'marksman',
+})
+
+require('lspconfig').lua_ls.setup({
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' }
       }
     }
-  })
+  }
+})
 
-  lsp.setup()
-else
-  print('lsp-zero not installed')
-end
+lsp.setup()
